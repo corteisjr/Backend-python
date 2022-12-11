b602ac6a13e71cdc4b794cf6b4575f14ef97dc99
arcenioCorteis <arcenioCorteis@github.com>
2022-12-11 08:19:24 +0200

    feat: Implementing select user in userRepository

--- src/infra/repo/user_repository.py
+++ src/infra/repo/user_repository.py
@@ -1,3 +1,4 @@
+from typing import List
 from src.domain.models import Users
 from src.infra.config import DBConectionHandler
 from src.infra.entities import Users as UsersModel
@@ -28,3 +29,51 @@ class UserRepository:
             finally:
                 db_connection.session.close()
             return None
+
+    @classmethod
+    def select_user(cls, user_id: int = None, name: str = None) -> List[Users]:
+        """Select data in user entitty by id and/or name
+
+        Args:
+            user_id (int, optional): _id of the register_. Defaults to None.
+            name (str, optional): _users name_. Defaults to None.
+
+        Returns:
+            List[Users]: _Users selected_
+        """
+
+        try:
+            query_data = None
+
+            if user_id and not name:
+
+                with DBConectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(id=user_id)
+                        .one()
+                    )
+                    query_data = [data]
+            elif not user_id and name:
+                with DBConectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(name=name)
+                        .one()
+                    )
+                    query_data = [data]
+            elif user_id and name:
+                with DBConectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(id=user_id, name=name)
+                        .one()
+                    )
+                    query_data = [data]
+            return query_data
+        except:
+            db_connection.session.rollback()
+            raise
+        finally:
+            db_connection.session.close()
+        return None
